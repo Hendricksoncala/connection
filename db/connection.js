70866df30700e60a28c9c7142baae1d942612c12
Hendricksoncala <156349949+Hendricksoncala@users.noreply.github.com>
2024-06-17 16:12:27 -0500

    feat: :rocket: Empezando crecion de consultas por nodeJS

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+import mysql from 'mysql2/promise';
+
+    export const connection = await mysql.createConnection({
+        host: 'roundhouse.proxy.rlwy.net',
+        user: 'root',
+        database: 'railway',
+        port: 48029,
+        password: "NsswzXBzwCLFOdwxgDPxClOIDkHTBYOk"
+      });
+    console.log("conexion de date de datos OK", connection);    
+
